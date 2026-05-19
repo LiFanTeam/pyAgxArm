a5d99241a5619230394a68dd8ceef51bc830cd63
jack <1506289682@qq.com>
2026-05-19 17:52:48 +0800

    fix(piper_x): V188版本关节4和5转向取反
    - move_mit位置速度力矩取反
    - move_cpv_pos位置取反
    
    ---
    
    fix(piper_x): Fixed the steering error issue in joints 4 and 5 in version V188
    
    - Inverts position, velocity, and torque in move_mit
    - Inverts position in move_cpv_pos

--- pyAgxArm/protocols/can_protocol/drivers/piper_x/versions/v188/driver.py
+++ pyAgxArm/protocols/can_protocol/drivers/piper_x/versions/v188/driver.py
@@ -1,3 +1,5 @@
+from typing_extensions import Literal
+
 from ....piper.versions.v188.driver import Driver as PiperDriverV188
 
 
@@ -28,4 +30,37 @@ class Driver(PiperDriverV188):
     - Some `set_*` APIs additionally verify by reading back state; their
       docstrings will mention the verification method if applicable.
     """
-    pass
+
+    def move_mit(
+        self,
+        joint_index: Literal[1, 2, 3, 4, 5, 6],
+        p_des: float = 0.0,
+        v_des: float = 0.0,
+        kp: float = 10.0,
+        kd: float = 0.8,
+        t_ff: float = 0.0,
+    ):
+        if joint_index in [4, 5]:
+            p_des = -p_des
+            v_des = -v_des
+            t_ff = -t_ff
+        super().move_mit(
+            joint_index,
+            p_des=p_des,
+            v_des=v_des,
+            kp=kp,
+            kd=kd,
+            t_ff=t_ff,
+        )
+
+    def move_cpv_pos(
+        self,
+        joint_index: Literal[1, 2, 3, 4, 5, 6],
+        pos: float,
+    ) -> None:
+        if joint_index in [4, 5]:
+            pos = -pos
+        super().move_cpv_pos(
+            joint_index,
+            pos,
+        )
